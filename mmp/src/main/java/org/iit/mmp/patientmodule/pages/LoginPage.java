0232df40e32b700d9a8a7018fae4afca8f0281af
sabitha <sabithag19@gmail.com>
2020-09-19 17:36:39 -0400

    Push

---
+++ mmp/src/main/java/org/iit/mmp/patientmodule/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package org.iit.mmp.patientmodule.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	WebDriver driver;
+	public LoginPage()
+	{
+		this.driver = driver;
+	
+	}
+	 
+
+}
