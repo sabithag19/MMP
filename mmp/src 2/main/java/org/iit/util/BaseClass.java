01a8bee0b29e35ef5420fe2a07094b275d56be5f
sabitha <sabithag19@gmail.com>
2020-09-19 16:54:53 -0400

    Pushing

---
+++ mmp/src 2/main/java/org/iit/util/BaseClass.java
@@ -0,0 +1,24 @@
+package org.iit.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	 
+		
+		 protected WebDriver driver ;
+		@BeforeTest
+		public void instantiateDriver()
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			
+		}
+		
+	 
+
+}
